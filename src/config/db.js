44740111767df9e06ae7cb180a3e9f09329558f9
duclbph33843 <duclbph33843@fpt.edu.vn>
2024-09-07 11:55:39 +0700

    Cap nhat 5

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
 export const connectDB = async (uri) => {
-    try {
-        await mongoose.connect(uri);
-        console.log("Kết nối thành công !");
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true, // Sử dụng URL phân tích mới
+      useUnifiedTopology: true, // Sử dụng mô hình mới để quản lý kết nối
+      // useCreateIndex: true, // Nếu bạn dùng các chỉ mục với MongoDB cũ, bỏ ghi chú nếu cần
+      // useFindAndModify: false, // Nếu bạn muốn dùng phương thức mới của MongoDB
+    });
+    console.log("Kết nối thành công!");
+  } catch (error) {
+    console.error("Lỗi kết nối MongoDB:", error.message);
+  }
 };
