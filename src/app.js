96f786a473cf4edf594fcadc1e6bcd8e321c14c0
duclbph33843 <duclbph33843@fpt.edu.vn>
2024-09-07 10:29:27 +0700

    Cap nhat

--- src/app.js
+++ src/app.js
@@ -24,4 +24,10 @@ app.use(`/api/v1/`, usersRouter);
 app.use(`/api/v1/`, categoryRouter);
 app.use(`/api/v1/`, cartRouter);
 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 export const viteNodeApp = app;
